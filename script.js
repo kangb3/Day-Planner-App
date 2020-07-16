ef360db96f3d49992d42b5d9de022f8ebf0f72d0
Baldeep Kang <bkang4612@gmail.com>
2020-07-15 18:17:06 -0700

    Add color code to time blocks for past or present

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ businessHours = [
         id: "0",
         hour: "09",
         time: "09",
-        timeOfDay: "AM",
+        timeOfDay: "am",
         input: ""
     },
     {
@@ -93,7 +93,7 @@ businessHours.forEach(function(hour) {
 
 
 //Used for task fields withon time block
-var hourTask = $("<div>").attr({"class": "col-md-9 description"});
+var hourTask = $("<div>").attr({"class": "col-md-9 description p-0"});
 
     var taskInfo = $("<textarea>");
 
@@ -102,12 +102,24 @@ var hourTask = $("<div>").attr({"class": "col-md-9 description"});
 
     taskInfo.attr("id", hour.id);
 
+    if (hour.time < moment().format("HH")) {
+        taskInfo.attr ({
+            "class": "past", 
+        })
+    } else if (hour.time === moment().format("HH")) {
+        taskInfo.attr({
+            "class": "present"
+        })
+    } else if (hour.time > moment().format("HH")) {
+        hour.attr({
+            "class": "future"
+        })
+    }
+
     var saveButton = $("<i class='fa fa-save'></i>")
 
 
-    var saveTask = $("<button>")
-    
-    .attr({"class": "col-md-1 saveBtn"});
+    var saveTask = $("<button>").attr({"class": "col-md-1 saveBtn"});
 
 
     saveTask.append(saveButton);
@@ -115,5 +127,43 @@ var hourTask = $("<div>").attr({"class": "col-md-9 description"});
     timeBlock.append(hourLabel, hourTask, saveTask)
 
 });
+
+
+
+function init() {
+    var savedHours = JSON.parse(localStorage.getItem("businessHours"));
+
+    if (savedHours) {
+        businessHours = savedHours;
+    }
+
+    saveTasks();
+    showTasks();
+}
+
+init();
+
+// stores user input to localStorage
+function saveTasks() {
+
+    localStorage.setItem("businessHours", JSON.stringify(businessHours));
+}
+
+function showTasks() {
+        businessHours.forEach(function (hour) {
+            $(`#${hour.id}`).val(hour.timeOfDay);
+        })
+    }
+    
+
+// saves data to be used in localStorage
+$(".saveBtn").on("click", function(event) {
+    event.preventDefault();
+    var saveIndex = $(this).siblings(".description").children(".future").attr("id");
+    businessHours[saveIndex].input = $(this).siblings(".description").children(".future").val();
+    console.log(saveIndex);
+    saveTasks();
+    showTasks();
+})
 
 
